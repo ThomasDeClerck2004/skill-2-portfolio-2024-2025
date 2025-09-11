1d4a0cbcb1cd315ace45814853a25ba08b89c6d6
Thomas De Clerck <thomas.de.clerck4@gmail.com>
2025-09-11 15:13:56 +0200

    added devcontainerization for later development, updated cv

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  server: {
+    host: '0.0.0.0', // listen on all interfaces inside container
+    port: 5173,      // ensure same port as forwarded in devcontainer.json
+  },
 })
